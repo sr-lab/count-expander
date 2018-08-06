4139365513b0d3334c7a5cce34240ccd0fd3219f
Saul Johnson <saul.a.johnson@gmail.com>
2018-08-06 12:18:33 +0100

    Minor docblock addition

--- CountExpander.Shared/FileUtils.cs
+++ CountExpander.Shared/FileUtils.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 namespace CountExpander.Shared
 {
+    /// <summary>
+    /// Contains file I/O helper methods.
+    /// </summary>
     public class FileUtils
     {
         /// <summary>
